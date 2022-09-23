29e520865d06ea1c7db66a1a3fe4d9a79fb739a0
Gonzalo Juarez <100467578@alumnos.uc3m.es>
2022-09-23 21:17:03 +0200

    order resource locations in filetree

--- audio-img-to-mp4.sh
+++ audio-img-to-mp4.sh
@@ -5,4 +5,4 @@ mp3=${2}
 out="${3}.mp4"
 len=${4}
 
-ffmpeg -loop 1 -i ${img} -i ${out} -t ${len} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest ${out}
+ffmpeg -loop 1 -i ${img} -i ${mp3} -t ${len} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest ${out}
---
+++ first-seconds-of-audio.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+audio=${1}
+out=${2}
+len=${3}
+
+ffmpeg -ss 0 -i ${audio} -t ${len} -c copy ${2}
