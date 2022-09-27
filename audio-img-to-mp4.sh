d23e321354d086ed2877a7af5de12627aeb3974e
Gonzalo Juarez <100467578@alumnos.uc3m.es>
2022-09-27 16:09:08 +0200

    create videos from 20sec samples and insert them in html

--- audio-img-to-mp4.sh
+++ audio-img-to-mp4.sh
@@ -2,7 +2,8 @@
 
 img=${1}
 mp3=${2}
-out="${3}.mp4"
-len=${4}
+out="${mp3/mp3/mp4}"
+out="${out/resources\/audios/resources\/videos}"
+len=${3}
 
-ffmpeg -loop 1 -i ${img} -i ${mp3} -t ${len} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest ${out}
+ffmpeg -loop 1 -i "${img}" -i "${mp3}" -t "${len}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${out}"
--- first-seconds-of-audio.sh
+++ first-seconds-of-audio.sh
@@ -4,4 +4,4 @@ audio=${1}
 out=${2}
 len=${3}
 
-ffmpeg -ss 0 -i ${audio} -t ${len} -c copy ${2}
+ffmpeg -ss 0 -i "${audio}" -t "${len}" -c copy "${2}"
