6df183d12634a4f32a1d1f42de9435c637872f72
Gonzalo Juarez <100467578@alumnos.uc3m.es>
2022-09-29 15:15:57 +0200

    format for delivery

--- audio-img-to-mp4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-img=${1}
-mp3=${2}
-out="${mp3/mp3/mp4}"
-out="${out/resources\/audios/resources\/videos}"
-len=${3}
-
-ffmpeg -loop 1 -i "${img}" -i "${mp3}" -t "${len}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "${out}"
--- first-seconds-of-audio.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-audio=${1}
-out=${2}
-len=${3}
-
-ffmpeg -ss 0 -i "${audio}" -t "${len}" -c copy "${2}"
