93d52de20872351366f5e8c5daccf9791e0a86f2
Gonzalo Juarez <100467578@alumnos.uc3m.es>
2022-10-12 18:24:13 +0200

    separar formulario de logIn y signIn en archivos

---
+++ ej/script/frontend/Form/logIn.js
@@ -0,0 +1,74 @@
+/**
+ * @param {function(Event): any}  onSuccess
+ * @return {function(Event): any}
+ */
+function onSubmitLogInHandler(onSuccess) {
+    return (e) => {
+        e.preventDefault();
+        onSuccess(e);
+    };
+}
+
+/** @readonly @type {string} */
+const LogInFormID = "LogIn";
+
+/** @readonly @type {InputData[]} */
+const LogInFieldsData = [
+    {
+        id: "username", label: "Your username", type: "text",
+        inputValidation: {
+            errorMsg: "This field is mandatory",
+            attributes: {
+                required: true,
+            }
+        },
+        extraAttributes: {
+            placeholder: "sounsound-user00"
+        }
+    },
+    {
+        id: "password", label: "Your password", type: "password",
+        inputValidation: {
+            errorMsg: "This field is mandatory",
+            attributes: {
+                required: true,
+                maxlength: 8,
+                pattern: "[a-z0-9A-Z]+"
+            }
+        },
+        extraAttributes: {
+            placeholder: "secret"
+        }
+    },
+];
+
+/** @readonly @type {ButtonData[]} */
+const LogInButtons = [
+    {
+        text: "Log In!",
+        classes: ["secondary-light-bg-color", "button"],
+        extraAttributes: {
+            type: "submit",
+        },
+        onClickHandler: undefined
+    },
+    {
+        text: "Clear Fields",
+        classes: ["main-light-bg-color", "button"],
+        extraAttributes: {
+            type: "reset",
+        },
+        onClickHandler: undefined,
+    },
+];
+
+/** @readonly @type {IconsListData} */
+const LogInIcons = {upperText: "or, log-in with ", iconsPath: [...OAuthIcons]};
+
+const logInForm = Form(LogInFieldsData, LogInButtons, LogInIcons, LogInFormID);
+logInForm.addEventListener("submit", onSubmitLogInHandler(closeModalClickHandler));
+
+logIn.addEventListener("click", setOpenModalHandler({
+    title: "Enter your credentials:",
+    content: logInForm,
+}));
---
+++ ej/script/frontend/Form/signIn.js
@@ -0,0 +1,124 @@
+/**
+ * @param {function(Event): any}  onSuccess
+ * @return {function(Event): any}
+ */
+function onSubmitSignInHandler(onSuccess) {
+    return (e) => {
+        e.preventDefault();
+        onSuccess(e);
+    };
+}
+
+/** @readonly @type {InputData[]} */
+const SignInFieldsData = [
+    {
+        id: "username", label: "Choose a username", type: "text",
+        inputValidation: {
+            errorMsg: "This field is mandatory.",
+            attributes: {
+                required: true
+            }
+        },
+        extraAttributes: {
+            placeholder: "sounsound-user00"
+        }
+    },
+    {
+        id: "password", label: "Choose a password (max 8 chars., lowercase or numbers)", type: "password",
+        inputValidation: {
+            errorMsg: "Write a password of at least 8 characters.",
+            attributes: {
+                required: true,
+                maxlength: 8,
+                pattern: "[a-z0-9A-Z]+"
+            }
+        },
+        extraAttributes: {
+            placeholder: "secret"
+        }
+    },
+    {
+        id: "firstName", label: "What's your first name?", type: "text",
+        inputValidation: {
+            errorMsg: "Enter your name (letters, dashes and upper commas).",
+            attributes: {
+                required: true,
+                pattern: "([a-zA-Z]|\-|')+"
+            }
+        },
+        extraAttributes: {
+            placeholder: "Leonardo"
+        }
+    },
+    {
+        id: "lastName", label: "What's your last name?", type: "text",
+        inputValidation: {
+            errorMsg: "Enter your last name (letters, dashes and upper commas).",
+            attributes: {
+                required: true,
+                pattern: "([a-zA-Z]|\-|')+"
+            }
+        },
+        extraAttributes: {
+            placeholder: "DiCaprio"
+        }
+    },
+    {
+        id: "email", label: "What's your email?", type: "email",
+        inputValidation: {
+            errorMsg: "Enter a mail (ex.: me@mail.com)",
+            attributes: {
+                required: true,
+                pattern: "[a-zA-Z0-9_.+-]+@[a-zA-Z0-9_.+-](.[a-z0-9]+)+"
+            }
+        },
+        extraAttributes: {
+            placeholder: "leodc@mail.com"
+        }
+    },
+    {
+        id: "birth", label: "Date of birth", type: "date",
+        inputValidation: {
+            errorMsg: "Please select a date",
+            attributes: {
+                required: true,
+            }
+        },
+        extraAttributes: undefined
+    },
+    {
+        id: "profilePic", label: "Upload a profile picture", type: "file",
+        inputValidation: undefined,
+        extraAttributes: undefined,
+    },
+];
+
+/** @readonly @type {string} */
+const SignInFormID = "SignIn";
+
+/** @readonly @type {ButtonData[]} */
+const SignInButtons = [
+    {
+        text: "Sign In!",
+        classes: ["secondary-light-bg-color", "button"],
+        extraAttributes: {type: "submit"},
+        onClickHandler: undefined,
+    },
+    {
+        text: "Clear Fields",
+        classes: ["main-light-bg-color", "button"],
+        extraAttributes: {type: "reset"},
+        onClickHandler: undefined,
+    },
+];
+
+/** @readonly @type {IconsListData} */
+const SignInIcons = {upperText: "or, sign-in with ", iconsPath: [...OAuthIcons]};
+
+const signInForm = Form(SignInFieldsData, SignInButtons, SignInIcons, SignInFormID);
+signInForm.addEventListener("submit", onSubmitSignInHandler(closeModalClickHandler));
+
+signIn.addEventListener("click", setOpenModalHandler({
+    title: "Register",
+    content: signInForm,
+}));
