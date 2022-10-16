1c64d051b1261dedd5b451f201ea502beb2b77e3
Gonzalo Juarez <gonzalojuarezte@gmail.com>
2022-10-16 17:53:00 +0200

    Navbar de usuario logueado sin dropdown menu

--- ej/script/frontend/MainNavbar.js
+++ ej/script/frontend/MainNavbar.js
@@ -11,10 +11,11 @@ class MainNavbar {
      * @return {HTMLElement}
      */
     render() {
+        removeAllChildren(this.element);
         if (__Store.state.loggedIn === null)
             return defaultOpts(this.element);
         else
-            return this.element;
+            return userOpts(this.element, __Store.state.loggedIn);
     }
 };
 
@@ -51,5 +52,51 @@ function defaultOpts(root) {
     return root;
 }
 
+/**
+ * 
+ * @param {HTMLElement} root 
+ * @param {User} user 
+ * @return {HTMLElement}
+ */
 function userOpts(root, user) {
+    const rightside = document.createElement("div");
+    rightside.classList.add("home-section");
+    rightside.innerHTML = `
+        <img alt="app image" src="./icons/icons8-video-playlist-40.png">
+        <h2>SoundSound</h2>
+    `;
+
+    const center = Searchbar();
+
+    const leftside = document.createElement("ul");
+    const userMenu = document.createElement("li");
+    const userImg = document.createElement("img");
+
+    // TODO: handle user image user.profilePicB64
+    userImg.src = "./icons/icons8-user-64.png";
+    userImg.addEventListener("click", () => {console.warn("Dropdown not implemented yet!!")})
+
+    leftside.appendChild(userMenu).appendChild(userImg);
+
+    root.appendChild(rightside);
+    root.appendChild(center);
+    root.appendChild(leftside);
+
+    return root;
+}
+
+/**
+ * @return {HTMLElement}
+ */
+function Searchbar() {
+    const ret = document.createElement("div");
+    ret.classList.add("searchbar");
+
+    const searchInput = document.createElement("input");
+    searchInput.placeholder = "Search your favourite songs";
+    searchInput.type = "text";
+
+    ret.appendChild(searchInput);
+
+    return ret;
 }
